e5531f90ef41fe0588acdbf970ec953dbdf3e632
GorkyAnge <GorkyAnge@hotmail.com>
2023-11-14 11:34:19 -0500

    Cambios del MainPage => HomePage

---
+++ ProductoAppMAUI/HomePage.xaml.cs
@@ -0,0 +1,22 @@
+namespace ProductoAppMAUI;
+
+public partial class HomePage : ContentPage
+{
+    int count = 0;
+    public HomePage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
--- ProductoAppMAUI/MauiProgram.cs
+++ ProductoAppMAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ProductoAppMAUI
 {
@@ -13,7 +14,7 @@ namespace ProductoAppMAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                }).UseMauiCommunityToolkit();
 
 #if DEBUG
 		builder.Logging.AddDebug();
