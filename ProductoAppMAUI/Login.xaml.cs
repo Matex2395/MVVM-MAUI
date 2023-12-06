f3655b086c47cfe46f3608090e7b6ae55853b45f
MateoRosero <mateorgafjo@gmail.com>
2023-12-06 10:42:17 -0500

    Agregando primera versión de Login

---
+++ ProductoAppMAUI/Login.xaml.cs
@@ -0,0 +1,19 @@
+namespace ProductoAppMAUI;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void OnClickLogIn(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnClickRegisterView(object sender, TappedEventArgs e)
+    {
+
+    }
+}
---
+++ ProductoAppMAUI/Register.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProductoAppMAUI;
+
+public partial class Register : ContentPage
+{
+	public Register()
+	{
+		InitializeComponent();
+	}
+}
